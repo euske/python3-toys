4c22fc87be4d22819afb38c1c5c2dfc3b3cba43c
Yusuke Shinyama <yusuke@shinyama.jp>
2021-10-04 11:28:14 +0900

    Changed: csv2html.py column selection support.

--- csv2html.py
+++ csv2html.py
@@ -11,33 +11,48 @@ def q(s):
             replace('"','&#34;').
             replace("'",'&#39;'))
 
+def sel(row, cols):
+    if not cols:
+        return row
+    else:
+        return [ row[i] for i in cols ]
+
 def main(argv):
     import getopt
     def usage():
-        print('usage: %s [-h] [-c codec] [-T title] [files ...]' % argv[0])
+        print(f'usage: {argv[0]} [-H] [-C encoding] [-T title] [-c cols] [files ...]')
         return 100
     try:
-        (opts, args) = getopt.getopt(argv[1:], 'hc:T:')
+        (opts, args) = getopt.getopt(argv[1:], 'HC:T:c:')
     except getopt.GetoptError:
         return usage()
-    header = False
+    header = True
     encoding = 'utf-8'
     title = ' '.join(args)
+    selection = []
     for (k, v) in opts:
-        if k == '-h': header = True
-        elif k == '-c': encoding = v
+        if k == '-H': header = False
+        elif k == '-C': encoding = v
         elif k == '-T': title = v
+        elif k == '-c':
+            for c in v.split(','):
+                if '-' in c:
+                    (c1,_,c2) = c.partition('-')
+                    for i in range(int(c1), int(c2)+1):
+                        selection.append(i)
+                else:
+                    selection.append(int(c))
     fp = fileinput.input(args, openhook=(lambda path, mode: open(path, mode, encoding=encoding)))
-    print('<html><head><title>%s</title><style>' % q(title))
+    print(f'<html><head><title>{q(title)}</title><style>')
     print('table { border-collapse: collapse; }')
     print('</style></head><body><table border>')
     for row in csv.reader(fp):
         if header:
-            cols = ( '<th>%s</th>' % q(col) for col in row )
+            cols = ( f'<th>{q(col)}</th>' for col in sel(row, selection) )
             header = False
         else:
-            cols = ( '<td>%s</td>' % q(col) for col in row )
-        print ('<tr>%s</tr>' % (''.join(cols)))
+            cols = ( f'<td>{q(col)}</td>' for col in sel(row, selection) )
+        print(f'<tr>{"".join(cols)}</tr>')
     print('</table></body></html>')
     return 0
 
